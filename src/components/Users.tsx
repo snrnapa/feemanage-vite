2be5d2c0552e3cee9c0858192035eb134f8dbaa5
snrnapa <shino.satoru@gmail.com>
2024-02-04 23:57:18 +0900

    Userの情報取得を完成させた

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -11,7 +11,7 @@ function Users() {
   const fetcher = (url: string): Promise<any> =>
     fetch(url).then((res) => res.json());
 
-  const { data, error } = useSWR(api_host + '/user', fetcher);
+  const { data, error, isLoading } = useSWR(api_host + '/user', fetcher);
 
   const testconsole = () => {
     console.log(data);
@@ -22,6 +22,30 @@ function Users() {
     <>
       <p className="text-2xl">Users Data</p>
       <button onClick={testconsole}>userのテストするで</button>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : error ? (
+        <p>failed to load</p>
+      ) : (
+        <div className="p-4  space-y-3 rounded overflow-hidden shadow-lg">
+          {data.map((user) => {
+            return (
+              <div
+                key={user.id}
+                className="flex flex-col justify-center rounded border  shadow-lg bg-cyan-100 "
+              >
+                <p className="text-xl">社員番号：{user.id}</p>
+                <p className="text-xl">
+                  {user.last_name} {user.first_name}
+                </p>
+                <p className="text-xl">
+                  dept/team : {user.dept_id}/{user.team_id}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
