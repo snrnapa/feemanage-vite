a8c46934a86c8ab93ee457b509e328b42cb48b5a
snrnapa <shino.satoru@gmail.com>
2024-02-04 23:30:02 +0900

    tailwindcssの環境を構築した

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <p className="text-9xl">hello vite tailwindcss</p>
+    </>
+  );
 }
 
 export default App;
